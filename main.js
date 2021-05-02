32c22e0f60ec82f4884f099f4107b0316bd06c5f
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-05-02 10:29:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,25 @@ recognition.onresult = function(event){
     console.log(event);
     var Content = event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=Content;
-    speak();
+    if (Content == "take my selfie"){
+        console.log("taking your selfie in 5 seconds");
+        speak();
+    }
 }
 
 function speak(){
     var synth = window.speechSynthesis;
-    var speak_data = document.getElementById("textbox").value;
+    var speak_data = "Taking your Selfie in 5 seconds";
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(
+        function(){
+            take_snapshot();
+            save();
+        },5000
+    );
 }
 
 camera = document.getElementById("camera");
@@ -28,3 +38,18 @@ Webcam.set({
     image_format:'png',
     png_quality:90
 });
+
+function take_snapshot(){
+    Webcam.snap(
+        function(data_uri){
+            document.getElementById("result").innerHTML='<img id="image_selfie" src="'+data_uri+'"/>';
+        }
+    );
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("image_selfie").src;
+    link.href = image;
+    link.click();
+}
